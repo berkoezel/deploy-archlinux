63273ed0f7e1dc194f0b3463aea79542e47ccb12
Berk Ozel <berkozel@protonmail.com>
2023-11-06 14:53:17 +0300

    Push /etc/hosts configurator

--- src/conf-hosts.sh
+++ src/conf-hosts.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-hostnamectl set-hostname archlinux
+. ./00_VARS.sh
+
+echo '127.0.0.1 localhost' > /etc/hosts
+echo '127.0.1.1' ${DEPLOYARCH_HOSTNAME} >> /etc/hosts
+
+echo '::1 localhost ip6-localhost ip6-loopback' >> /etc/hosts
