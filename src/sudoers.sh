b15cf61bdaebf5fc14d6494ed5688c5c8ebfbd19
Berk Ozel <berkozel@protonmail.com>
2023-11-06 13:34:21 +0300

    Update sudoers script

--- src/sudoers.sh
+++ src/sudoers.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# To write berk's permissions under root's permissions in the file for readability reasons.
+# To write user's permissions under root's permissions in the file for readability reasons.
 LINENUM=$(grep root -n /etc/sudoers | grep ALL | cut -d':' -f 1)
 sed -i '${LINENUM}i\berk ALL=(ALL:ALL) ALL' /etc/sudoers
