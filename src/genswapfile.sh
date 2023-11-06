895aba05a022893ee2fd92db2a8418270af42926
Berk Ozel <berkozel@protonmail.com>
2023-11-06 14:38:48 +0300

    Push initial genswapfile script

---
+++ src/genswapfile.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+. ./00_VARS.sh
+dd if=/dev/zero of=${DEPLOYARCH_SWAPLOCATION} bs=1024 count=$((1024*${DEPLOYARCH_SWAPFILE_SIZE})) status=progress
+
+chmod 600 ${DEPLOYARCH_SWAPLOCATION}
+mkswap ${DEPLOYARCH_SWAPLOCATION}
+swapon ${DEPLOYARCH_SWAPLOCATION}
+
+echo ${DEPLOYARCH_SWAPLOCATION} 'swap swap defaults 0 0' >> /etc/fstab
