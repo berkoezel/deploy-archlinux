1a327710d2bbac89ee053e0e39948ebeb6ca02a1
Berk Ozel <berkozel@protonmail.com>
2023-11-06 15:11:26 +0300

    Push locales generator

---
+++ src/setlocales.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+. ./00_VARS.sh
+
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+
+echo 'en_US ISO-8859-1' >> /etc/locale.gen
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+
+locale-gen
