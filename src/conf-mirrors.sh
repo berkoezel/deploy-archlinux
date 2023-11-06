ff4d0af05c85652c0feb1584860c6786b4237abb
Berk Ozel <berkozel@protonmail.com>
2023-11-06 14:44:15 +0300

    Push mirrorlist configurator

---
+++ src/conf-mirrors.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+. ./00_VARS.sh
+
+curl https://archlinux.org/mirrorlist/?country=${DEPLOYARCH_MIRROR_COUNTRY}&protocol=http&protocol=https&ip_version=4 | sed 's/#Server/Server/' > /etc/pacman.d/mirrorlist
