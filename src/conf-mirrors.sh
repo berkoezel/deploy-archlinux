8e7607d5e067854b302df84f009dbf7b324cb8fe
Berk Ozel <berkozel@protonmail.com>
2023-11-10 16:28:41 +0300

    Hardcode mirror location to Switzerland

--- src/conf-mirrors.sh
+++ src/conf-mirrors.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-. ./00_VARS.sh
-
-curl -s 'https://archlinux.org/mirrorlist/?country=${DEPLOYARCH_MIRROR_COUNTRY}&protocol=http&protocol=https&ip_version=4' | sed 's/#Server/Server/' > /etc/pacman.d/mirrorlist
-
+curl -s 'https://archlinux.org/mirrorlist/?country=CH&protocol=http&protocol=https&ip_version=4' | sed 's/#Server/Server/' > /etc/pacman.d/mirrorlist
 pacman -Sy
