369ca85eb4cf3d57fe6d02e9f5dd7aa36c75ca44
Berk Ozel <berkozel@protonmail.com>
2023-11-13 21:10:56 +0300

    Enable parallel downloading option on pacman

--- src/conf-mirrors.sh
+++ src/conf-mirrors.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+sed -i 's/#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 curl -s 'https://archlinux.org/mirrorlist/?country=CH&protocol=http&protocol=https&ip_version=4' | sed 's/#Server/Server/' > /etc/pacman.d/mirrorlist
 pacman -Sy
