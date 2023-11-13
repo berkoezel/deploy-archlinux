d4c8caedbb46ee3a0298a204edeaba97132281a0
Berk Ozel <berkozel@protonmail.com>
2023-11-13 20:50:33 +0300

    Minor changes

--- chroot-handler.sh
+++ chroot-handler.sh
@@ -6,7 +6,7 @@
 SCRIPTS_TARGET_DIR='/mnt/chroot-scripts'
 mkdir $SCRIPTS_TARGET_DIR
 
-FILES=(00_VARS.sh settimezone.sh setlocales.sh sethostname.sh users.sh conf-hosts.sh enable-services.sh sudoers.sh genswapfile.sh set-aurhelper.sh set-firewall.sh set-hostname.sh initramfs.sh grub.sh)
+FILES=(00_VARS.sh settimezone.sh setlocales.sh sethostname.sh users.sh conf-hosts.sh enable-services.sh sudoers.sh genswapfile.sh set-aurhelper.sh set-firewall.sh set-hostname.sh initramfs.sh bootloader.sh)
 
 for file in ${FILES[@]}; do
 	cp ./$i $SCRIPTS_TARGET_DIR
