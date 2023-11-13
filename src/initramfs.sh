a9c839bcd8deecf53f78ba970f82b883f04a184e
Berk Ozel <berkozel@protonmail.com>
2023-11-13 23:15:01 +0300

    Minor changes

--- src/initramfs.sh
+++ src/initramfs.sh
@@ -2,5 +2,5 @@
 
 . ./00_VARS.sh
 
-sed -i 's/consolefont block/consolefont ${DEPLOYARCH_INITRD_HOOKS} block' /etc/mkinitcpio.conf 
+sed -i "s/consolefont block/consolefont ${DEPLOYARCH_INITRD_HOOKS} block/" /etc/mkinitcpio.conf 
 mkinitcpio -P
