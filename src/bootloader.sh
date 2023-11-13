23ec07c4180a9d9f590ccf6e9bfc6374813defc4
Berk Ozel <berkozel@protonmail.com>
2023-11-13 23:13:52 +0300

    Fix variable assignation

--- src/bootloader.sh
+++ src/bootloader.sh
@@ -4,8 +4,8 @@
 
 pacman -S grub efibootmgr os-prober
 
-TARGET_LINE$(grep CMDLINE_LINUX_DEFAULT /etc/default/grub -n | cut -d : -f 1)
-sed -i '${TARGET_LINE}d' /etc/default/grub
+TARGET_LINE=$(grep CMDLINE_LINUX_DEFAULT /etc/default/grub -n | cut -d : -f 1)
+sed -i "${TARGET_LINE}d" /etc/default/grub
 
 echo "" >> /etc/default/grub
 echo "CMDLINE_LINUX_DEFAULT=${DEPLOYARCH_KERNEL_PARAMS}" >> /etc/default/grub
