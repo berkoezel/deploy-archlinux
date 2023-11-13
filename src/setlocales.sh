e546d1790666813a6e918ef2c0c28b209e951822
Berk Ozel <berkozel@protonmail.com>
2023-11-13 23:16:35 +0300

    Add Turkish locales

--- src/setlocales.sh
+++ src/setlocales.sh
@@ -7,4 +7,7 @@ echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 echo 'en_US ISO-8859-1' >> /etc/locale.gen
 echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
 
+echo 'tr_TR ISO-8859-9' >> /etc/locale.gen
+echo 'tr_TR.UTF-8 UTF-8' >> /etc/locale.gen
+
 locale-gen
