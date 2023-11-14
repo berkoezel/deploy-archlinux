4f2d721b92b75ff6aae4e1fc859e3fd31c8b9716
Berk Ozel <berkozel@protonmail.com>
2023-11-14 13:25:28 +0300

    Refresh keyring before repository update

--- src/conf-mirrors.sh
+++ src/conf-mirrors.sh
@@ -2,4 +2,5 @@
 
 sed -i 's/#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 curl -s 'https://archlinux.org/mirrorlist/?country=CH&protocol=http&protocol=https&ip_version=4' | sed 's/#Server/Server/' > /etc/pacman.d/mirrorlist
+pacman-key --init
 pacman -Sy
